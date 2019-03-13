7358570183d13a3c9e6a7cd20e039f73c635d9ba
Zack Siri <zacksiri@gmail.com>
2019-03-13 22:31:47 +0700

    added tailwind successfully

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react'
+import c from 'classnames'
 import { render } from 'react-dom'
 
 import * as styles from './index.sass'
 
 class Index extends React.PureComponent {
   render() {
-    return (<div className={styles.red}>
+    return (<div className={c('bg-black', styles.white)}>
       artellectual.com
     </div>)
   }
