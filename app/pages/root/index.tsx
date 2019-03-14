b3148c0f02f96f256dbdad4d4d3499d57a7b2606
Zack Siri <zacksiri@gmail.com>
2019-03-14 19:28:36 +0700

    added type for RootProps

--- app/pages/root/index.tsx
+++ app/pages/root/index.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react'
+import { RouteNode } from 'react-router5'
 import c from 'classnames'
 
 import * as styles from './index.sass'
 
-class Root extends React.PureComponent {
+import { RouteNodeState } from 'route-node'
+
+interface RootProps {
+  route: RouteNodeState
+}
+
+class Root extends React.PureComponent<RootProps> {
   render() {
     return (
       <div className={c('bg-black', styles.white)}>
@@ -13,4 +20,8 @@ class Root extends React.PureComponent {
   }
 }
 
-export default Root
+export default (props: { route: RouteNodeState }) => (
+  <RouteNode nodeName="">
+    {({route}) => <Root route={route} {...props} />}
+  </RouteNode>
+)
